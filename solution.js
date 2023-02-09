1f28dc4a9da297c42bb815a2def49f8577e3fca2
Manoumey <manuella.meyer@dci.education>
2023-02-09 15:36:44 +0100

    2.Commit

--- solution.js
+++ solution.js
@@ -16,14 +16,11 @@
 
 
 function factorial(n){
-if(n<0){
-    return "Positive Werte bitte!!";
-} else if(n===0) {
+
+ if(n<=0)
     return 1;
-}
-else{
 return n*factorial(n-1);
 }
-}
 
-console.log(factorial(-5));
+
+console.log(factorial(5));
